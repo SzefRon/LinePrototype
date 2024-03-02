3d6653ecd4c899acd016071b2c57cdf61453b944
Paweł Stus <pawel.j.stus@gmail.com>
2024-03-02 12:12:05 +0100

    Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{ 
+    [SerializeField] Transform player1;
+    [SerializeField] Transform player2;
+
+    [SerializeField] float smoothingFactor;
+
+    Vector3 midpoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        midpoint = Vector3.Lerp(player1.position, player2.position, 0.5f);
+        midpoint.y = transform.position.y;
+        transform.position = Vector3.Lerp(transform.position, midpoint, smoothingFactor);
+    }
+}
