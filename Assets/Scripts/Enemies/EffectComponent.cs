63a0a6449ec9ea7f405061a42389476a4ca170fd
SzefRon <92161300+SzefRon@users.noreply.github.com>
2024-03-11 18:43:25 +0100

    Effect component

---
+++ Assets/Scripts/Enemies/EffectComponent.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectComponent : MonoBehaviour
+{
+    private HealthComponent healthComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthComponent = GetComponent<HealthComponent>();
+    }
+
+    public void TakeRopeEffect(SegmentUpgrades upgrade)
+    {
+        switch (upgrade)
+        {
+            case SegmentUpgrades.Fire:
+                Debug.Log("Fire");
+                float dmg = healthComponent.MaxHealthFraction(10);
+                healthComponent.TakeDamageOverTime(dmg, 1.0f, 3);
+                break;
+            case SegmentUpgrades.Electricity:
+                Debug.Log("Electricity");
+                break;
+            case SegmentUpgrades.Laser:
+                Debug.Log("Laser");
+                break;
+            case SegmentUpgrades.Shredder:
+                Debug.Log("Shredder");
+                break;
+            case SegmentUpgrades.Mirror:
+                Debug.Log("Mirror");
+                break;
+            case SegmentUpgrades.Explosive:
+                Debug.Log("Explosive");
+                break;
+            case SegmentUpgrades.Lifesteal:
+                Debug.Log("Lifesteal");
+                break;
+            case SegmentUpgrades.Shield:
+                Debug.Log("Shield");
+                break;
+            case SegmentUpgrades.Slime:
+                Debug.Log("Slime");
+                break;
+            default:
+                Debug.Log("None");
+                break;
+        }
+    }
+}
--- Assets/Scripts/Enemies/HealthComponent.cs
+++ Assets/Scripts/Enemies/HealthComponent.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class HealthComponent : MonoBehaviour
 {
+    [SerializeField] private float maxHealth;
+    Coroutine DOTcoroutine;
     public float Health
     {
         get { return health; }
-        set { health = value; }
     }
     private float health;
 
     void Start()
+    {       
+        health = maxHealth;
+    }
+
+    public float MaxHealthFraction(int fraction)
     {
-        
-    }}
+        return maxHealth * fraction;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void TakeDamageOverTime(float damage, float delay, int ticks)
+    {
+        if (DOTcoroutine != null)
+        {
+            StopCoroutine(DOTcoroutine);
+        }
+        DOTcoroutine = StartCoroutine(DamageOverTime(damage, delay, ticks));
+    }
+
+    private IEnumerator DamageOverTime(float damage, float delay, int ticks)
+    {
+        for (int i = 0; i < ticks; i++)
+        {
+            TakeDamage(damage);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
