cd0e185ab14092d91b6abef68c5509ab5ef2c000
Paweł Stus <pawel.j.stus@gmail.com>
2024-03-11 18:21:03 +0100

    Nowa kamera

--- Assets/Scripts/Miscellaneous/CameraController.cs
+++ Assets/Scripts/Miscellaneous/CameraController.cs
@@ -6,7 +6,8 @@ public class CameraController : MonoBehaviour
     [SerializeField] Transform player2;
 
     [SerializeField] float smoothingFactor;
-
+    [SerializeField] float height;
+    
     Vector3 midpoint;
 
     // Update is called once per frame
@@ -15,5 +16,8 @@ public class CameraController : MonoBehaviour
         midpoint = Vector3.Lerp(player1.position, player2.position, 0.5f);
         midpoint.y = transform.position.y;
         transform.position = Vector3.Lerp(transform.position, midpoint, smoothingFactor);
+        transform.position += -1.0f * height * Vector3.forward;
+        midpoint.y = 1.0f;
+        transform.LookAt(midpoint);
     }
 }
